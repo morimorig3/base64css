81e9b0fd8bc20dc10a72f0a0a16c68cdff2591a9
morimorig3 <mori.nto@outlook.jp>
2021-08-14 20:45:21 +0900

    README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Base64 background-imageジェネレーター
 
 ドラッグアンドドロップでSVGからbackground-imageプロパティを生成します。
+
+[デモ](https://6117a98fd4ac5aa1f0d54053--hungry-fermi-3088d8.netlify.app/)
+
+## スクリーンショット
+
+<img src="https://user-images.githubusercontent.com/63396746/129445200-52f448fe-ca44-47ac-ac08-5deec59fad18.png" width="500">
--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Layout from 'components/layout';
 import Base64CssGen from 'components/base64cssgen';
+import { FaGithub } from 'react-icons/fa';
 const App = () => {
   return (
     <Layout>
-      <h1 className="text-lg font-bold pb-2 border-b-2 border-gray-400 border-dashed mb-4">
+      <h1 className="text-lg font-bold pb-2 border-b-2 border-gray-400 border-dashed mb-4 flex justify-between">
         Base64 background-imageジェネレーター
+        <a
+          href="https://github.com/morimorig3/base64css"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub size="1.5em" />
+        </a>
       </h1>
       <Base64CssGen />
     </Layout>
