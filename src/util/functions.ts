c26a72d02b6e9ac965ed8a59bb734645db15aacb
morimorig3 <mori.nto@outlook.jp>
2021-12-04 09:40:26 +0900

    Missing return type on function  @typescript-eslint/explicit-module-boundary-typeのエラーが出ていたので返り値の型を指定

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,7 +1,7 @@
 // 対応拡張子かチェック
 export const isMatchExtend = (string: string): boolean => /.+\.(jpg|png|svg|gif)/i.test(string);
 
-export const getDataURLAsync = (file: File) =>
+export const getDataURLAsync = (file: File): Promise<string | ArrayBuffer | null> =>
   // eslint-disable-next-line implicit-arrow-linebreak
   new Promise<string | ArrayBuffer | null>((resolve, reject) => {
     const reader = new FileReader();
