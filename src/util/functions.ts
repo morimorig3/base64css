d911711bfd31a387369c59cdaef3cf395b81a109
morimorig3 <mori.nto@outlook.jp>
2021-12-05 11:01:24 +0900

    eslint-disable-next-line implicit-arrow-linebreak回避のために関数の記述方法を変更

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,12 +1,16 @@
 // 対応拡張子かチェック
 export const isMatchExtend = (string: string): boolean => /.+\.(jpg|png|svg|gif)/i.test(string);
 
-export const getDataURLAsync = (file: File): Promise<string | ArrayBuffer | null> =>
-  // eslint-disable-next-line implicit-arrow-linebreak
-  new Promise<string | ArrayBuffer | null>((resolve, reject) => {
+type getDataURLAsync = (file: File) => Promise<string | ArrayBuffer | null>;
+
+export const getDataURLAsync: getDataURLAsync = async (file) => {
+  const promise = await new Promise<string | ArrayBuffer | null>((resolve, reject) => {
     const reader = new FileReader();
 
     reader.onload = () => resolve(reader.result);
     reader.readAsDataURL(file);
     reader.onerror = reject;
   });
+
+  return promise;
+};
