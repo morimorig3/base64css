66eb57d907608a13cdc5fad48edc09341b3a162b
morimorig3 <mori.nto@outlook.jp>
2022-01-08 10:49:23 +0900

    tailwindCSSと依存パッケージをアンインストール

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from 'App';
-import './index.css';
 
 const theme = extendTheme({
   styles: {
